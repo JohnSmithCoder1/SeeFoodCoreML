e1181b00532aaa0b4f3925e8f7382582f7600617
Banana Viking <bananavikingcoder@gmail.com>
2019-01-20 16:38:00 -0600

    classify userPickedImage

--- SeeFoodCoreML/ViewController.swift
+++ SeeFoodCoreML/ViewController.swift
@@ -33,9 +33,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = userPickedImage
             
-            guard let ciImage = CIImage(image: userPickedImage) else {
+            guard let userCIImage = CIImage(image: userPickedImage) else {
                 fatalError("Could not convert UIImage to CIImage.")
             }
+            
+            detect(image: userCIImage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
@@ -44,6 +46,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func detect(image: CIImage) {
         guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
             fatalError("Loading CoreML model failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image.")
+            }
+
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
         }
     }
 }
