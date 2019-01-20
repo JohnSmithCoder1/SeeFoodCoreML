e8bafcab83f707b7b9852c5d9496ab2e33d06d8e
Banana Viking <bananavikingcoder@gmail.com>
2019-01-20 16:16:49 -0600

    added imagePicker delegate function and camera and photo library descriptions to info.plist

--- SeeFoodCoreML/ViewController.swift
+++ SeeFoodCoreML/ViewController.swift
@@ -27,5 +27,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker.sourceType = .camera
         imagePicker.allowsEditing = false
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
 }
 
